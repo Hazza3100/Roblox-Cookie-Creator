cc7d3f481a21f7d191e3d5ad09c5ad3ecead3878
Hazza <86918289+Hazza3100@users.noreply.github.com>
2022-11-20 13:21:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import threading
 import skidbilly as pystyle
 
 from colorama import Fore
-
+from itertools import cycle
 
 
 with open('data/config.json') as f:
@@ -67,7 +67,17 @@ class stat():
     err     = 0
 
 
+class Misc:
+    def __init__(self):
+        pass
 
+    def get_cookie():
+        with open('data/cookies.txt', 'r') as f:
+            cookies = [line.strip('\n') for line in f]
+        return cookies
+     
+    cookie = get_cookie()
+    ilit_cookies = cycle(cookie)
 
 
 class aio:
@@ -83,6 +93,10 @@ class aio:
                 print(e)
             else:
                 None
+
+    def csrfFriend(cookie):
+        r = requests.post('https://auth.roblox.com/v2/logout', headers={'cookie': f'.ROBLOSECURITY={cookie}','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'})
+        return r.headers.get('x-csrf-token')
 
 
     def GetUser(self, username):
@@ -169,6 +183,27 @@ class aio:
             else:
                 None
 
+    def friend(self, userId):
+        try:
+            cookie = next(Misc.ilit_cookies)
+            headers = {'authority': 'friends.roblox.com','accept': 'application/json, text/plain, */*','accept-language': 'en-GB,en;q=0.9','content-type': 'application/json;charset=utf-8','cookie': f'.ROBLOSECURITY={cookie}','origin': 'https://web.roblox.com','referer': 'https://web.roblox.com/','sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"Windows"','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-site','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36','x-csrf-token': aio.csrfFriend(cookie),}
+            response = self.session.post(f'https://friends.roblox.com/v1/users/{userId}/request-friendship', headers=headers)
+            try:
+                if response.json()['success'] == True:
+                    print(f"{Fore.BLUE}[ {Fore.GREEN}+ {Fore.BLUE}]{Fore.RESET} Successfully Followed")
+            except:
+                pass
+            try:
+                if response.json()['errors'][0]['code'] == 5:
+                    print(f"{Fore.BLUE}[ {Fore.RED}x {Fore.BLUE}]{Fore.RESET} Request already sent")
+            except:
+                pass
+        except Exception as e:
+            if debug == True:
+                print(e)
+            else:
+                pass
+
 
 
 class menu:
@@ -186,6 +221,7 @@ class menu:
         print("\n\n")
 
         print(f'                                              {Fore.RED}[{Fore.RESET} {Fore.BLUE}1{Fore.RESET} {Fore.RED}]{Fore.RESET} Account Creator')
+        print(f'                                              {Fore.RED}[{Fore.RESET} {Fore.BLUE}2{Fore.RESET} {Fore.RED}]{Fore.RESET} Friend User')
 
 
         print("\n\n\n")
@@ -209,6 +245,28 @@ class menu:
                 x.join()
             time.sleep(2.2)
             input(f"{Fore.RED}[{Fore.RESET}{Fore.BLUE}Cookie Creator{Fore.RESET}{Fore.RED}]{Fore.RESET} Completed tasks! Press Enter To Return To The Menu {Fore.YELLOW}>{Fore.RESET} ")
+            menu().main()
+
+        if choice == 2:
+            os.system('cls')
+            pystyle.Write.Print("""
+                                            ╔═╗┬─┐┬┌─┐┌┐┌┌┬┐
+                                            ╠╣ ├┬┘│├┤ │││ ││
+                                            ╚  ┴└─┴└─┘┘└┘─┴┘""", pystyle.Colors.purple_to_blue, interval=0)
+            print("")
+            username = input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] Username {Fore.CYAN}> {Fore.WHITE}")
+            threads = input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] Requests to send {Fore.CYAN}> {Fore.WHITE}")
+
+            userID = aio().GetUser(username)
+
+            for i in range(int(threads)):
+                x = threading.Thread(target=aio().friend, args=(userID,))
+                x.start()
+
+            for i in range(int(threads)):
+                x.join()
+            time.sleep(2.2)
+            input(f"{Fore.RED}[{Fore.RESET}{Fore.BLUE}Friend User{Fore.RESET}{Fore.RED}]{Fore.RESET} Completed tasks! Press Enter To Return To The Menu {Fore.YELLOW}>{Fore.RESET} ")
             menu().main()
 
 
